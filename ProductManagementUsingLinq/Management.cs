5d29402aa5a9d217f31b9784d3c0602f7ca8de51
senpooja <poojasen744055@gmail.com>
2022-09-08 16:56:33 +0530

    UC-2-TopRatingRecords

--- ProductManagementUsingLinq/Management.cs
+++ ProductManagementUsingLinq/Management.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProductManagementSystemUC_1
+namespace ProductManagementSystemUsingLinq
 {
     public class Management
     {
